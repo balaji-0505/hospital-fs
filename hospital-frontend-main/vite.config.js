8df7be3c8319752f5c14cc9e1c6d5afa6ac9bd75
balaji-0505 <pabbathibalaji12@gmail.com>
2025-10-25 18:03:16 +0530

    Update vite.config.js

--- hospital-frontend-main/vite.config.js
+++ hospital-frontend-main/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hospital-fs/'  // match the repo name in GitHub Pages URL
+  base: '/hospital-fs/'   // <-- add this (your repo name)
 });
